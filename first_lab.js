917de3c8fe15caea109150106cba838c6280948b
Jason Spiller <jason@thetargetlab.com>
2018-03-21 15:29:49 -0600

    Rearranging folders

---
+++ first_lab.js
@@ -0,0 +1,100 @@
+/*
+
+// fixed output
+console.log("Making your way in the world today takes everything you've got.");
+console.log("Taking a break from all your worries, sure would help a lot.");
+console.log("Wouldn't you like to get away?");
+console.log("Sometimes you want to go");
+console.log("Where everybody knows your name,");
+console.log("and they're always glad you came.");
+console.log("You wanna be where you can see,");
+console.log("our troubles are all the same");
+console.log("You wanna be where everybody knows");
+console.log("Your name.");
+
+
+// orginal answers
+//  999 > 999 false
+//  999 == 999 true
+//  999 != 999 false
+//  -5 >= -4 false
+//  100 <= -100 false
+//  20 + 5 < 5 false
+//  81 / 9 == 9 true
+//  9 != 8 + 1 false
+
+// testing answers
+console.log(999 > 999);
+console.log(999 == 999);
+console.log(999 != 999);
+console.log(-5 >= -4);
+console.log(100 <= -100);
+console.log(20 + 5 < 5);
+console.log(81 / 9 == 9);
+console.log(9 != 8 + 1);
+
+
+// while loop that prints 'Ginger chocolate honey patties'
+hungry = 0;
+
+while (hungry < 1000) {   
+	console.log('Ginger chocolate honey patties');
+	hungry++;
+}
+
+
+// while loop that output incrementor
+i = 0;
+
+while (i <= 1000) {   
+	console.log(i);
+	i++;
+}
+
+
+// while loop that uses interprolation
+i = 0;
+
+while (i <= 1000) {   
+	console.log("Current loop number is: " + i);
+	i++;
+}
+
+
+// for loop that counts from 0 to 100
+for(let i=1; i <= 100; i++) {
+	console.log(i);
+}
+
+
+// for loop that starts 7 and ends at 635
+for(i=7; i <= 635; i++) {
+	console.log(i);
+}
+
+
+// for loop using variables
+let start = 0;
+const limit = 100;
+
+for (start; start < limit; start++) {
+	console.log(start + " is still less than " + limit);
+}
+
+
+// for loop of a real world situation
+let shots = 0;
+const drunk = 12;
+
+for (shots; shots < drunk; shots++) {
+	if (shots < 5) {
+		console.log("I've only had " + shots + " shots, lets take a double.");
+		shots++;
+	} else {
+		console.log("I've already had " + shots + " shots, but I can take another.");
+	}
+}
+
+console.log("I'm drunk!");
+
+*/
---
+++ homework2_answers.md
@@ -0,0 +1,22 @@
+Part 1
+
+1. What command do you use to setup a git repository inside of your folder? git init
+2. What command do you use to ask git to start tracking a file? git add
+3. What command do you use to ask git to move your file from the staging area to the repository? git commit
+
+1. What command do you use to pull any changes from the master repository into your local repository? git checkout
+2. What command do you use to unstage a file? git reset
+3. What command do you use to change your files back to how they were after a commit? git checkout
+4. Why is it important to use -- when changing files back to a previous state? So we target a specific file and not the whole project.
+5. Why might you want to reset your files back to a previous commit? To get rid of other people's changing that introduced bugs.
+
+1. What command do you use to create a branch? git branch
+2. What command do you use to use a different branch? git checkout
+3. Why would you want to use a branch other than the default master? To add a new feature or a bug fix.
+
+1. Give an example for when you would use git merge and give an example for when it would be better to submit a pull request to have your branch merged. Use git merge when you need to merge your own working into the master. A pull request is more appropriate when you have forked a project and you need the developer to review your branch. 
+2. What command do you use to send all of the work that you've done locally to your remote repository? git push origin master
+
+
+Part 2
+
